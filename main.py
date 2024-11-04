12c2ac7082441f785bb15547701a6219beed32b0
Beastnard <john.m.bernard@outlook.com>
2024-11-04 15:49:33 -0500

    Added character count with symbols

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_chars = get_num_chars(text)
     print(f"{num_words} words found in the document")
+    print(f"dictionary of character counts: {num_chars}")
 
 
 def get_num_words(text):
@@ -13,7 +15,22 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+
+def get_num_chars(text):
+    lower_case = text.lower()
+    char_count = {}
+
+    for char in lower_case:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    return dict(sorted(char_count.items()))
 
 
+    
 main()
+
 
