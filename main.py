ba922b021e6d9794fbc206af9d8f484d3b347048
Beastnard <john.m.bernard@outlook.com>
2024-11-07 16:09:28 -0500

    generated report

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
+    list_dicts = get_list_dicts(num_chars)
+    report = get_report(list_dicts)
     print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
     print("")
-    print(f"dictionary of character counts: {num_chars}")
+    for key, value in report:
+        print(f"The {key} character was found {value} times")
+    print("--- End report ---")
 
 
 def get_num_words(text):
@@ -32,6 +36,21 @@ def get_num_chars(text):
 
     return dict(sorted(char_count.items()))
 
+
+def get_list_dicts(num_chars):
+    
+    list_of_dicts = []
+
+    for key, value in num_chars.items():
+        list_of_dicts.append({key: value})
+        
+    return list_of_dicts
+
+def get_report(list_dicts):
+
+    for dict in list_dicts:
+        for key, value in dict.items():
+            yield key, value
 
     
 main()
